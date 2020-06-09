c7652fc609dae95bb867f67dc25ceec168714fcb
babataku <babataku@fsi.co.jp>
2020-06-09 11:00:32 +0900

    在宅勤務申請実施の追加の修正、翌月の勤怠入力処理(未完了)

--- put_comapny.js
+++ put_comapny.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 const setting = {
   sleep: 200,
-  next: false,  // 前の月に翌月の勤怠をいれるときtrue 
+  next: true,  // 前の月に翌月の勤怠をいれるときtrue 
   domain: true, // ドメインの外からアクセス
   user: '',     // ドメインの外からアクセスするときのユーザー名
   pass: '',     // ↑のパスワード
@@ -44,16 +44,19 @@ const setting = {
     await page.goto('https://www.honsha.fsi.co.jp/cws/cws?@SID=null&@SUB=root.cws.shuro.personal.term_kinmu_input&@SN=root.cws.shuro.personal.term_kinmu_input&@FN=form_shuro&@ACTION_LOG_TXT=%E5%8B%A4%E6%80%A0%E5%AE%9F%E7%B8%BE%E3%80%80%E5%85%A5%E5%8A%9B%3Cbr%3E%3Cbr%3E', { waitUntil: 'domcontentloaded' }); 
   }
 
-  // move to next (翌月の勤怠をいれるときは↓のコメントアウトを解除する)
-  // await page.waitFor('a[id="TONXTTM"]', {timeout: 5000});
-  // await page.click('a[id="TONXTTM"]')
+  // move to next (翌月の勤怠をいれるときは↓のコメントアウトを解除する)現在の月と入力した月を比較して、次の月だったときだけ、やるようにifで分岐したい
+  if(setting.next){
+    await page.waitFor('a[id="TONXTTM"]', {timeout: 5000});
+   await page.click('a[id="TONXTTM"]')
+  }
+   
 
-  const mon = 6;
+  const mon = 7;
   const year = 2020;
   const list = [
-   /*月*/ {day: 1 , start:'8:30', end:'19:00', works: [{code:'PGK801', time:'9:30'}]},
+   /*月*/ {day: 1 , start:'9:00', end:'17:30', works: [{code:'GQ3X02', time:'7:30'}],zaitaku:true},
 //    /*火*/ {day: 2 , start:'9:00', end:'18:00', works: [{code:'PGKC01', time:'8:00'}]},
-//    /*水*/ {day: 3 , start:'9:00', end:'18:00', works: [{code:'PGZC01', time:'7:00'}, {code: 'PGJS01', time:'1:00'}]},
+//   /*水*/ {day: 3 , start:'9:00', end:'18:00', works: [{code:'PGZC01', time:'7:00'}, {code: 'PGJS01', time:'1:00'}]},
 //    /*木*/ {day: 4 , start:'9:00', end:'18:00', works: [{code:'PLAB01', time:'7:00'}, {code: 'GL8L04', time:'1:00'}]},
 //    /*金*/ {day: 5 , start:'9:00', end:'18:00', works: [{code:'PGRK01', time:'8:00'}]},
 //    /*月*/ {day: 8 , start:'9:00', end:'18:00', works: [{code:'PGRK01', time:'8:00'}]},
@@ -67,7 +70,7 @@ const setting = {
 //    /*木*/ {day: 18, start:'9:00', end:'18:00', works: [{code:'PGSD01', time:'7:00'}, {code: 'GL8L04', time:'1:00'}]},
 //    /*金*/ {day: 19, start:'9:00', end:'18:00', works: [{code:'PGSD01', time:'8:00'}]},
 //    /*月*/ {day: 22, start:'9:00', end:'18:00', works: [{code:'PGSD01', time:'8:00'}]},
-//    /*火*/ {day: 23, start:'9:00', end:'18:00', works: [{code:'PGYU01', time:'8:00'}]},
+//   /*火*/ {day: 23, start:'9:00', end:'18:00', works: [{code:'PGYU01', time:'8:00'}]},
 //    /*水*/ {day: 24, start:'9:00', end:'18:00', works: [{code:'PGYU01', time:'8:00'}]},
 //    /*木*/ {day: 25, start:'9:00', end:'18:00', works: [{code:'PGYU01', time:'7:00'}, {code: 'GL8L04', time:'1:00'}]},
 //    /*金*/ {day: 26, start:'9:00', end:'18:00', works: [{code:'PGYU01', time:'8:00'}]},
@@ -134,7 +137,8 @@ const setting = {
     
      //在宅勤務実施
      if(list[i].zaitaku == true){
-      await page.select('select[name="GI_COMBOBOX13_Seq0S"]','2',{timeout: 5000});
+      await page.waitFor('input[name="GI_TIMERANGE14_Seq0STH"]',{timeout: 5000});
+      await page.select('select[name="GI_COMBOBOX13_Seq0S"]','2',);
       await page.$eval('input[name="GI_TIMERANGE14_Seq0STH"]', (el, val) => el.value = val, sH);
       await page.$eval('input[name="GI_TIMERANGE14_Seq0STM"]', (el, val) => el.value = val, sM);
       await page.$eval('input[name="GI_TIMERANGE14_Seq0ETH"]', (el, val) => el.value = val, eH);
