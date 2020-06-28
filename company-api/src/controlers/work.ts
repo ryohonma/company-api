8a49f1f5bb65b1df05286889d9cea283c49fd1f9
ryohonma <ryohonma0830@gmail.com>
2020-06-29 00:46:43 +0900

    apiからpuppeteerが動かせるようにした npm installしなおしてね

--- company-api/src/controlers/work.ts
+++ company-api/src/controlers/work.ts
@@ -1,10 +1,27 @@
 import express from 'express';
+import puppeteer from 'puppeteer';
 
 const router = express.Router();
 
 router.get('/', async (req:express.Request, res:express.Response) => { 
-  // ここでpuppeterrのgetを呼び出す
-    return res.status(200).json({result:0});
+  const browser = await puppeteer.launch({
+    args: ['--disable-dev-shm-usage'],
+    headless: false 
+  }); 
+
+  const url = 'http://www.honsha.fsi.co.jp';
+  console.log(url);
+
+  //　タブの生成
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'domcontentloaded' }); 
+
+  // タブを移動
+  const pages = await browser.pages();
+  const detailPage = pages[1]
+  await detailPage.bringToFront();
+
+  return res.status(200).json({result:0});
 });
 
 router.post('/', async (req:express.Request, res:express.Response) => { 
